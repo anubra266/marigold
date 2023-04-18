cd0092afa2a488fbe157e6f9dcfddba4cfaed51e
sebald <sebastian.sebald@gmail.com>
2023-04-18 07:39:44 +0000

    Deploying to gh-pages from @ marigold-ui/marigold@dab01d7fef59af7cb824e47890ade0e85b73b7dd 🚀

---
+++ _next/static/chunks/pages/_error-ed015b6fa2334819.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4820],{1459:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(6590)}])}},function(n){n.O(0,[9774,2888,179],(function(){return _=1459,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
