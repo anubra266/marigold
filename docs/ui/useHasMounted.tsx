39edd8464d0de60d72268a7a72b64e9f3edb3210
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-04-11 10:27:48 +0200

    docs: clean up `useHasMounted`

--- docs/ui/useHasMounted.tsx
+++ docs/ui/useHasMounted.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
+/**
+ * Use this if you only want to render something on the client.
+ * Stolen from https://www.joshwcomeau.com/snippets/react-hooks/use-has-mounted/
+ */
 export const useHasMounted = () => {
-  const [hasMounted, setHasMounted] = React.useState(false);
-  React.useEffect(() => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
     setHasMounted(true);
   }, []);
+
   return hasMounted;
 };
